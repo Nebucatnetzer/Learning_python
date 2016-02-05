d36a14dd427733de8290e979f92e288b85aeaf4e
Andreas <andreas@2li.ch>
2016-02-05 13:24:56 +0100

    finished the vowel excersise

--- vowel/vowel.py
+++ vowel/vowel.py
@@ -1,2 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+vowels = ["a", "e", "i", "o", "u"] 
+letter = input("Please enter a letter.")
+
+if letter in vowels:
+    print("Your letter is a vowel.")
+else:
+    print("Your letter is a consonant.")
